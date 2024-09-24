239f36cfe285d81b271cdc228495e2382de7b8a7
Asma <saidiasma422@gmail.com>
2024-09-24 21:41:33 +0200

    🐛 FIX: Correct the background color

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    public function index(CategorieRepository $categorieRepository): Response
     {
+        $categories = $categorieRepository->findBy([],["nom" => "ASC"]);
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'categories' => 'categories',
         ]);
+    }
+
+    #[Route('/categorie/{id}', name: 'show_categorie')]
+    public function show(Categorie $categorie): Response
+    {
+        return $this->render('categorie/show.html.twig', [
+            'categorie' => $categorie
+
+        ]);
+
     }
 }
