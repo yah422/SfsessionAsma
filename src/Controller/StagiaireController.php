af74845df12887a1461b728110ffc3d939155da8
Asma <saidiasma422@gmail.com>
2024-09-26 10:31:01 +0200

    📦 NEW: Create new file to add stagiaire and correct the stagiaireController

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class StagiaireController extends AbstractController
 {
     #[Route('/stagiaire', name: 'app_stagiaire')]
     public function index(StagiaireRepository $stagiaireRepository): Response
     {
-        $stagiaires = $stagiaireRepository->findBy([],["nom" => "ASC"]);
+        $stagiaires = $stagiaireRepository->findBy([], ["nom" => "ASC"]);
         return $this->render('stagiaire/index.html.twig', [
             'stagiaires' => $stagiaires,
         ]);
@@ -24,8 +28,40 @@ class StagiaireController extends AbstractController
     {
         return $this->render('stagiaire/show.html.twig', [
             'stagiaire' => $stagiaire,
-
         ]);
+    }
 
+    #[Route('/stagiaire/supprimer/{id}', name: 'delete_stagiaire', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Stagiaire $stagiaire, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Stagiaire supprimé avec succès !');
+        
+        return $this->redirectToRoute('app_stagiaire');
+    }
+
+    #[Route('/stagiaire/ajouter', name: 'add_stagiaire')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $stagiaire = new Stagiaire();
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Stagiaire ajouté avec succès !');
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render('stagiaire/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('tel')
+            ->add('dateNaissance', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('session', EntityType::class, [
+                'class' => Session::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('sessions', EntityType::class, [
+                'class' => Session::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
