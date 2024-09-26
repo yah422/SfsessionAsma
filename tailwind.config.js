d8be4876c2e303d9d2097084716f29debcfa1aea
Asma <saidiasma422@gmail.com>
2024-09-26 16:48:13 +0200

    📦 NEW: Add new config for tailwind and switch bootstrap to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",],
   theme: {
     extend: {},
   },
