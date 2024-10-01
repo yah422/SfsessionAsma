a22216a2e6fca5e23f4e359d91e534d8fae60ea6
Asma <saidiasma422@gmail.com>
2024-10-01 20:48:00 +0200

    🐛 FIX: Correct the add StagiaireType and add.html.twig for stagiaire part

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -8,25 +8,22 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom',TextType::class)
-            ->add('prenom')
-            ->add('email')
-            ->add('tel')
-            ->add('dateNaissance', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('sessions', EntityType::class, [
-                'class' => Session::class,
-                'choice_label' => 'nom',
-                'multiple' => true,
-            ])
+        ->add('email', EmailType::class)
+        ->add('nom', TextType::class)
+        ->add('prenom', TextType::class)
+        ->add('tel', TextType::class)
+        ->add('dateNaissance', DateType::class, [
+            'widget' => 'single_text',
+        ])
         ;
     }
 
