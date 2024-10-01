ffe50e13f7c40724bd4952fdf5b2f4083de1bd64
Asma <saidiasma422@gmail.com>
2024-10-01 16:55:27 +0200

    🐛 FIX: Add a textType tot he nom in stagiareType

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -4,17 +4,18 @@ namespace App\Form;
 
 use App\Entity\Session;
 use App\Entity\Stagiaire;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class StagiaireType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom',TextType::class)
             ->add('prenom')
             ->add('email')
             ->add('tel')
