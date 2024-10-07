192dec7681f5c64e72b3faec1696a1b01dcbb92b
Asma <saidiasma422@gmail.com>
2024-10-07 08:58:49 +0200

    📦 NEW: Add a new file categorieType to add a categorie function in the controller

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +22,33 @@ class CategorieController extends AbstractController
             'categories' => $categories,
         ]);
     }
+
+       // Méthode pour ajouter une catégorie
+       #[Route('/categorie/ajouter', name: 'add_categorie')]
+       public function add(Security $security,Request $request, EntityManagerInterface $entityManager): Response
+       {
+   
+           // Vérification du rôle 'ROLE_ADMIN'
+           if (!$security->isGranted('ROLE_ADMIN')) {
+               // Rediriger vers une page d'erreur si l'utilisateur n'a pas le rôle 'ROLE_ADMIN'
+               return $this->render('session/errorPage.html.twig');     
+           }
+   
+           $categorie = new Categorie();
+           $form = $this->createForm(CategorieType::class,$categorie);
+   
+           $form->handleRequest($request);
+           if ($form->isSubmitted() && $form->isValid()) {
+               $entityManager->persist($categorie);
+               $entityManager->flush();
+   
+               return $this->redirectToRoute('app_categorie');
+           }
+   
+           return $this->render('categorie/add.html.twig', [
+               'form' => $form->createView(),
+           ]);
+       }
 
     #[Route('/categorie/{id}', name: 'show_categorie')]
     public function show(Categorie $categorie): Response
