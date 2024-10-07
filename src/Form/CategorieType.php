ac4d576cc9fbba74ab511de4afdd5c9902ca5c1f
Asma <saidiasma422@gmail.com>
2024-10-07 09:01:18 +0200

    📦 NEW: Add precision to the builder for categorieType

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -6,14 +6,18 @@ use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategorieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-        ;
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
